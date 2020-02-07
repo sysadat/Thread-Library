d1ac104dfbcb8592000d153da39850cf030914ff
Sayed Sadat <sysadat@ucdavis.edu>
2020-02-06 20:22:01 -0800

    Modify makefile and start test preempt

--- progs/test_preempt.c
+++ progs/test_preempt.c
@@ -4,7 +4,21 @@
 #include <uthread.h>
 #include <preempt.h>
 
+int thread1(void* arg)
+{
+	while(1) {
+		printf("At thread1\n");
+	}
+	return 1;
+}
+
 int main (void)
 {
+	int ret;
+	uthread_t tid;
+
+	tid = uthread_create(thread1, NULL);
+	uthread_join(tid, &ret);
+	printf("Back to main\n");
 	return 0;
 }
