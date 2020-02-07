621ec5b6aaf8352e05eafbcdc397f3fb34994931
Muhammed Halbutogullari <Muhammedali360@gmail.com>
2020-02-06 20:18:44 -0800

    Added Queue test

--- progs/uthread_checkTID.d
+++
@@ -1,3 +0,0 @@
-uthread_checkTID.o: uthread_checkTID.c ../libuthread/uthread.h \
- ../libuthread/uthread.c ../libuthread/context.h ../libuthread/uthread.h \
- ../libuthread/preempt.h ../libuthread/queue.h
